5ea4a06548ffe2f7f4a4b276d4108968578ebd78
ordinarylinuxuser <kentparker666@gmail.com>
2024-02-23 11:23:00 +0800

    Update the config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,15 @@
 return { --- Uncomment these if you want to manage LSP servers from neovim
     { 'williamboman/mason.nvim' },
     { 'williamboman/mason-lspconfig.nvim' },
+    { "Hoffs/omnisharp-extended-lsp.nvim", },
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
         config = function()
             vim.g.lsp_zero_extend_lspconfig = 1
             vim.g.lsp_zero_extend_cmp = 1
-
+            local pid = vim.fn.getpid()
+            local mason_path = '/home/ken/.local/share/nvim/mason/'
             local lsp_zero = require('lsp-zero')
 
             lsp_zero.on_attach(function(client, bufnr)
@@ -16,6 +18,7 @@ return { --- Uncomment these if you want to manage LSP servers from neovim
                 lsp_zero.default_keymaps({ buffer = bufnr })
                 lsp_zero.buffer_autoformat()
             end)
+
             -- sign icons
             lsp_zero.set_sign_icons({
                 error = '',
@@ -23,11 +26,56 @@ return { --- Uncomment these if you want to manage LSP servers from neovim
                 hint = '',
                 info = ''
             })
+
+            -- mason setup
             require('mason').setup({})
             require('mason-lspconfig').setup({
-                ensure_installed = { 'lua_ls' },
+                ensure_installed = { 'lua_ls', 'omnisharp' },
                 handlers = {
                     lsp_zero.default_setup,
+                    omnisharp = function()
+                        require('lspconfig').omnisharp.setup {
+                            cmd = { mason_path .. "bin/omnisharp", '--languageserver', '--hostPID', tostring(pid) },
+
+                            -- Enables support for reading code style, naming convention and analyzer
+                            -- settings from .editorconfig.
+                            enable_editorconfig_support = true,
+
+                            -- If true, MSBuild project system will only load projects for files that
+                            -- were opened in the editor. This setting is useful for big C# codebases
+                            -- and allows for faster initialization of code navigation features only
+                            -- for projects that are relevant to code that is being edited. With this
+                            -- setting enabled OmniSharp may load fewer projects and may thus display
+                            -- incomplete reference lists for symbols.
+                            enable_ms_build_load_projects_on_demand = false,
+
+                            -- Enables support for roslyn analyzers, code fixes and rulesets.
+                            enable_roslyn_analyzers = false,
+
+                            -- Specifies whether 'using' directives should be grouped and sorted during
+                            -- document formatting.
+                            organize_imports_on_format = false,
+
+                            -- Enables support for showing unimported types and unimported extension
+                            -- methods in completion lists. When committed, the appropriate using
+                            -- directive will be added at the top of the current file. This option can
+                            -- have a negative impact on initial completion responsiveness,
+                            -- particularly for the first few completion sessions after opening a
+                            -- solution.
+                            enable_import_completion = false,
+
+                            -- Specifies whether to include preview versions of the .NET SDK when
+                            -- determining which version to use for project loading.
+                            sdk_include_prereleases = true,
+
+                            -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+                            -- true
+                            analyze_open_documents_only = false,
+                            handlers = {
+                                ["textDocument/definition"] = require('omnisharp_extended').handler
+                            }
+                        }
+                    end
                 },
             })
 
