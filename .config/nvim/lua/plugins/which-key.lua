bdcb78b5fc02a21b5ca20e6aef4517cd301169ae
ordinarylinuxuser <kentparker666@gmail.com>
2025-03-05 08:52:53 +1100

    updated which key config

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,9 +2,29 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        preset = "modern",
+        spec = {
+            { "<leader>w",  "<cmd>update!<CR>",                                     desc = "Save" },
+            { "<leader>q",  "<cmd>q!<CR>",                                          desc = "Quit" },
+
+            -- Buffers
+            { "<leader>b",  group = "Buffers" },
+            { "<leader>bc", "<cmd>bd!<CR>",                                         desc = "Close Current Buffer" },
+            { "<leader>bD", "<Cmd>%bd|e#|bd#<CR>",                                  desc = "Delete All Buffers" },
+
+            -- Find
+            { "<leader>f",  group = "Find" },
+            { "<leader>ff", "<cmd>lua require('plugins.fzf_lua').find_files()<CR>", desc = "Find Files" },
+            { "<leader>fb", "<cmd>FzfLua buffers<CR>",                              desc = "Find Buffers" },
+            { "<leader>fo", "<cmd>FzfLua oldfiles<CR>",                             desc = "Find Old Files" },
+            { "<leader>fg", "<cmd>FzfLua live_grep<CR>",                            desc = "Live Grep" },
+            { "<leader>fc", "<cmd>FzfLua commands<CR>",                             desc = "Find Commands" },
+            { "<leader>fe", "<cmd>NvimTreeToggle<CR>",                              desc = "Explorer" },
+
+            -- Git
+            { "<leader>gs", "<cmd>Git<CR>",                                         desc = "Git Status" },
+
+        }
     },
     keys = {
         {
