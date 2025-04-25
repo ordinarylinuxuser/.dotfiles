e540606a7269cfaca4104058e48ccfd3249f1345
ordinarylinuxuser <kentparker666@gmail.com>
2025-04-25 14:37:11 +1000

    updated connect work avd script

--- scripts/connect_workavd.sh
+++ scripts/connect_workavd.sh
@@ -1,1 +1,12 @@
-xfreerdp3 workavd.rdpw /gateway:type:arm /sec:aad /floatbar +dynamic-resolution +f +auto-reconnect $@
+read -p "Client: " client
+VIVA_RDPW_LOCATION="VivaVm.rdpw"
+OTR_RDPW_LOCATION="OTRVm.rdpw"
+if [ "$client" = "viva" ]; then
+    xfreerdp $VIVA_RDPW_LOCATION /gateway:type:arm /sec:aad /floatbar /f +auto-reconnect +dynamic-resolution
+fi
+
+if [ "$client" = "otr" ]; then
+    read -p "User:" user
+    read -s -p "Password:" pass
+    xfreerdp $OTR_RDPW_LOCATION /gateway:type:arm /u:$user /p:$pass /d:peregrine.local /sec:tls /cert:ignore /floatbar /f +auto-reconnect +dynamic-resolution
+fi
