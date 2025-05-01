8df51b2001e6358b62661fe12ac6cb766c03e44b
ordinarylinuxuser <kentparker666@gmail.com>
2025-05-02 07:50:26 +1000

    updated configs

--- .bash_profile
+++ .bash_profile
@@ -11,7 +11,14 @@ export QT_QPA_PLATFORMTHEME=qt6ct
 export XMODIFIERS=@im=fcitx
 export DOTNET_ROOT=$HOME/.dotnet
 export DOTNET_INSTALL_DIR=$HOME/.dotnet
+export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+export QT_DEVICE_PIXEL_RATIO=2
+export QT_AUTO_SCREEN_SCALE_FACTOR=true
+
+export RESOLVE_SCRIPT_API="/opt/resolve/Developer/Scripting"
+export RESOLVE_SCRIPT_LIB="/opt/resolve/libs/Fusion/fusionscript.so"
+export PYTHONPATH="$PYTHONPATH:$RESOLVE_SCRIPT_API/Modules/"
 
 export PATH=$DOTNET_ROOT:$DOTNET_ROOT/tools:$HOME/bin:$HOME/.local/bin:/usr/local/bin:$HOME/go/bin:$HOME/.cargo/bin:$PATH
-source $HOME/.nvm/init-nvm.sh # for fedora
-#source /usr/share/nvm/init-nvm.sh # for arch
+#source $HOME/.nvm/init-nvm.sh # for fedora
+source /usr/share/nvm/init-nvm.sh # for arch
--- scripts/connect_workavd.sh
+++ scripts/connect_workavd.sh
@@ -1,12 +1,17 @@
 read -p "Client: " client
 VIVA_RDPW_LOCATION="VivaVm.rdpw"
 OTR_RDPW_LOCATION="OTRVm.rdpw"
+FREERDP="xfreerdp"
+if grep -q '^ID=arch$' /etc/os-release; then
+    FREERDP="xfreerdp3" #for arch use the xfreerdp3 cli
+fi
+
 if [ "$client" = "viva" ]; then
-    xfreerdp $VIVA_RDPW_LOCATION /gateway:type:arm /sec:aad /floatbar /f +auto-reconnect +dynamic-resolution
+    $FREERDP $VIVA_RDPW_LOCATION /gateway:type:arm /sec:aad /floatbar +auto-reconnect +dynamic-resolution
 fi
 
 if [ "$client" = "otr" ]; then
     read -p "User:" user
     read -s -p "Password:" pass
-    xfreerdp $OTR_RDPW_LOCATION /gateway:type:arm /u:$user /p:$pass /d:peregrine.local /sec:tls /cert:ignore /floatbar /f +auto-reconnect +dynamic-resolution
+    $FREERDP $OTR_RDPW_LOCATION /gateway:type:arm /u:$user /p:$pass /d:peregrine.local /sec:tls /cert:ignore /floatbar +auto-reconnect +dynamic-resolution
 fi
