5b098fb1aeedb9217480467f085dcd81eeb10573
ordinarylinuxuser <kentparker666@gmail.com>
2025-06-17 10:51:41 +1000

    update connect vm

--- scripts/connect_vm.sh
+++ scripts/connect_vm.sh
@@ -7,20 +7,15 @@ if grep -q '^ID=arch$' /etc/os-release; then
     FREERDP="xfreerdp3" #for arch use the xfreerdp3 cli
 fi
 
-if [ "$client" = "viva" ]; then
-    $FREERDP $VIVA_RDPW_LOCATION /gateway:type:arm /sec:aad /floatbar /f +auto-reconnect +dynamic-resolution
-fi
-
-if [ "$client" = "otr" ]; then
-    $FREERDP $OTR_RDPW_LOCATION /gateway:type:arm /u:$OTR_USER /p:$OTR_PASS /d:$OTR_DOMAIN /sec:tls /cert:ignore /floatbar +auto-reconnect +dynamic-resolution /f
-fi
-
-if [ "$client" = "win11" ]; then
-    VM_NAME=$WIN11_VM_NAME # Replace with your virtual machine's name
-    LIBVIRT_DEFAULT_URI="--connect qemu:///system"
+connect_vm_rdp() {
+    local VM_NAME=$1
+    local IP=$2
+    local USER=$3
+    local PASS=$4
+    local LIBVIRT_DEFAULT_URI="--connect qemu:///system"
 
     # Get the current state of the VM using virsh
-    state=$(virsh $LIBVIRT_DEFAULT_URI domstate "$VM_NAME" 2>/dev/null)
+    local state=$(virsh $LIBVIRT_DEFAULT_URI domstate "$VM_NAME" 2>/dev/null)
 
     # Check if the state does not include "running"
     if [[ "$state" != *"running"* ]]; then
@@ -38,5 +33,23 @@ if [ "$client" = "win11" ]; then
         done
     fi
 
-    $FREERDP /v:${WIN11_VM_IP} /u:${WIN11_USER} /p:${WIN11_PASS} /audio-mode:0 /monitors:0 /audio:sys:alsa /mic:sys:alsa /floatbar /cert:ignore /f +dynamic-resolution +auto-reconnect /auto-reconnect-max-retries:10 -compression +video
+    $FREERDP /v:${IP} /u:${USER} /p:${PASS} /audio-mode:0 /monitors:0 /audio:sys:alsa /mic:sys:alsa /floatbar /cert:ignore /f +dynamic-resolution +auto-reconnect /auto-reconnect-max-retries:10 -compression +video
+    
+    return 0 
+}
+
+if [ "$client" = "viva" ]; then
+    $FREERDP $VIVA_RDPW_LOCATION /gateway:type:arm /sec:aad /floatbar /f +auto-reconnect +dynamic-resolution
+fi
+
+if [ "$client" = "otr" ]; then
+    $FREERDP $OTR_RDPW_LOCATION /gateway:type:arm /u:$OTR_USER /p:$OTR_PASS /d:$OTR_DOMAIN /sec:tls /cert:ignore /floatbar +auto-reconnect +dynamic-resolution /f
+fi
+
+if [ "$client" = "win11" ]; then
+    connect_vm_rdp $WIN11_VM_NAME $WIN11_VM_IP $WIN11_USER $WIN11_PASS
+fi
+
+if [ "$client" = "archlinux" ]; then
+    connect_vm_rdp $ARCH_VM_NAME $ARCH_VM_IP $ARCH_USER $ARCH_PASS
 fi
